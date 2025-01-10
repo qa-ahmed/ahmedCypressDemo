c6d773677520359e743ced9b5f0f028073409eac
qa-ahmed <devasif1001@gmail.com>
2025-01-09 18:28:46 -0600

    Uncaught Exception fix

--- cypress/e2e/fluenceDemo/FirstTest.cy.ts
+++ cypress/e2e/fluenceDemo/FirstTest.cy.ts
@@ -1,6 +1,6 @@
 describe('Homepage Test Suite', () => {
   it('Homepage Test Suite', () => {
     cy.visit('https://fluenceenergy.com/');
-    cy.title().should('eq', 'Fluence');
+    cy.title().should('eq', 'Fluence | A Siemens and AES Company');
   })
 })
